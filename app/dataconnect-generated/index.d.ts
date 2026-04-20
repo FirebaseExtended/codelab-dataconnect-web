c887a7ea43b83fc79e1ee04d4c18cad05121eefc
Cynthia Wang <hyacccint@gmail.com>
2026-04-20 06:29:55 -0400

    feat: added schema

--- app/dataconnect-generated/index.d.ts
+++ app/dataconnect-generated/index.d.ts
@@ -11,6 +11,27 @@ export type DateString = string;
 
 
 
+export interface Emoji_Key {
+  id: UUIDString;
+  __typename?: 'Emoji_Key';
+}
+
+export interface Event_Key {
+  id: UUIDString;
+  __typename?: 'Event_Key';
+}
+
+export interface PriceHistory_Key {
+  id: UUIDString;
+  __typename?: 'PriceHistory_Key';
+}
+
+export interface StockOwnership_Key {
+  userId: string;
+  emojiId: UUIDString;
+  __typename?: 'StockOwnership_Key';
+}
+
 export interface User_Key {
   id: string;
   __typename?: 'User_Key';
